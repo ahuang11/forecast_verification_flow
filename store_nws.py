13581925e86efd6cb8bfdef365b7a5755055f51f
ahuang11 <andrew.h@prefect.io>
2022-03-24 19:13:21 -0600

    Add deployment

--- store_nws.py
+++ store_nws.py
@@ -1,32 +1,73 @@
+import sqlite3
 import xml.etree.cElementTree as et
 from collections import defaultdict
 from io import StringIO
+from datetime import timedelta
 
 import pandas as pd
 import requests
 from prefect import flow, task
+from prefect.tasks import task_input_hash
+from prefect.orion.schemas.schedules import IntervalSchedule
+from prefect.deployments import DeploymentSpec
 
-URL_FMT = (
-    "https://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}&FcstType=digitalDWML"
+
+META_URL = (
+    "https://mesonet.agron.iastate.edu/sites/networks.php?"
+    "network=_ALL_&format=csv&nohtml=on"
 )
+DATA_URL_FMT = "https://forecast.weather.gov/MapClick.php?lat={lat}&lon={lon}&FcstType=digitalDWML"
+TEMPORAL_COLS = ("start_valid_time", "initialization_time", "forecast_hour")
+DATABASE_NAME = "nws_forecast.db"
+STATION_IDS = ("KSEA", "KBDU", "KORD", "KCMI", "KMRY", "KSAN", "KNYC", "KIND", "KHOU")
+
+
+@task(cache_key_fn=task_input_hash, cache_expiration=timedelta(days=365))
+def retrieve_meta():
+    meta_df = pd.read_csv(META_URL)
+    return meta_df
+
+
+@task(cache_key_fn=task_input_hash)
+def get_station_coords(meta_df, stid):
+    lon, lat = meta_df.loc[meta_df["stid"] == stid, ["lon", "lat"]].iloc[0]
+    return lon, lat
 
 
 @task
-def retrieve_data(lat, lon):
-    url = URL_FMT.format(lat=lat, lon=lon)
+def retrieve_data(lon, lat):
+    url = DATA_URL_FMT.format(lon=lon, lat=lat)
     with requests.get(url) as resp:
-        content = resp.content.decode()
-    return content
+        data = resp.content.decode()
+    return data
 
 
 @task
-def parse_xml(content):
-    with StringIO(content) as buf:
+def get_root(data):
+    with StringIO(data) as buf:
         tree = et.parse(buf)
-
     root = tree.getroot()
+    return root
+
+
+@task
+def extract_initialization_time(root):
+    for head in root.findall("head"):
+        for product in head.findall("product"):
+            for creation_date in product.findall("creation-date"):
+                initialization_time = [creation_date.text]
+    return initialization_time
+
+
+@task
+def check_if_exists(initialization_time):
+    return False
+
+
+@task
+def extract_params(root):
     mapping = defaultdict(lambda: [])
-    for data in root.findall("data"):
+    for data in root.findall('data'):
         for time_layout in data.findall("time-layout"):
             for start_valid_time in time_layout.findall("start-valid-time"):
                 mapping["start_valid_time"].append(start_valid_time.text)
@@ -55,28 +96,56 @@ def parse_xml(content):
 
 
 @task
-def create_df(mapping):
-    df = (
-        pd.DataFrame(mapping)
-        .pipe(
-            lambda df: df.assign(
-                **{"start_valid_time": pd.to_datetime(df["start_valid_time"])}
-            )
-        )
-        .set_index("start_valid_time")
-        .apply(pd.to_numeric, errors="ignore")
-    )
+def create_df(mapping, initialization_time):
+    df = pd.DataFrame(mapping).pipe(
+        lambda df: df.assign(**{
+            "initialization_time": pd.to_datetime(initialization_time),
+            "start_valid_time": pd.to_datetime(df["start_valid_time"])
+        })
+    ).pipe(
+        lambda df: df.assign(**{
+            "forecast_hour": (
+                df["initialization_time"] - df["start_valid_time"]
+            ).total_seconds() / 3600
+        })
+    ).set_index(
+        ["start_valid_time", "initialization_time"]
+    ).apply(
+        pd.to_numeric,
+        errors="ignore"
+    ).reset_index()
     return df
+
+
+@task
+def to_database(stid, df):
+    with sqlite3.connect(DATABASE_NAME) as con:
+        df.to_sql(stid, con, index=False, if_exists="append")
+        for col in TEMPORAL_COLS:
+            con.execute(f"CREATE INDEX {col}_index ON {stid}({col});")
 
 
 @flow
-def process_forecast(lat, lon):
-    content = retrieve_data(lat, lon)
-    mapping = parse_xml(content)
-    df = create_df(mapping)
-    return df
+def process_forecast(stid):
+    stid = stid.upper()
+
+    meta_df = retrieve_meta()
+    lon, lat = get_station_coords(meta_df, stid).result()
+
+    data = retrieve_data(lon, lat)
+    root = get_root(data)
+    initialization_time = extract_initialization_time(root)
+    exists = check_if_exists(initialization_time)
+    if not exists:
+        mapping = extract_params(root)
+        df = create_df(mapping, initialization_time)
+        to_database(stid, df)
 
 
-lat = 40.0157
-lon = -105.2792
-process_forecast(lat, lon)
+DeploymentSpec(
+    flow=process_forecast,
+    name="process-forecast-hourly",
+    parameters={"stid": STATION_IDS},
+    tags=["nws", "forecast"],
+    schedule=IntervalSchedule(interval=timedelta(hours=1)),
+)
